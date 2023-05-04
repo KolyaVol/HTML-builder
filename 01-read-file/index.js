775b98d616eaced27123860ee21923e84687b097
VolNa <grizli887@mail.ru>
2023-05-04 18:03:28 +0300

    feat: solved first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "text.txt");
+const readStream = fs.createReadStream(filePath);
+readStream.on("data", (chunk) => {
+  try {
+    console.log(chunk.toString());
+  } catch (error) {}
+});
