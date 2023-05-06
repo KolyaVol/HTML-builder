01a9163ad2491cc56bb93fbc8ea8628e25f20425
VolNa <grizli887@mail.ru>
2023-05-06 19:38:43 +0300

    complited forth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const copyDir = async (src, dest) => {
+  await fs.mkdir(dest, { recursive: true });
+  const entries = await fs.readdir(src, { withFileTypes: true });
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await fs.copyFile(srcPath, destPath);
+    }
+  }
+};
+
+const srcDir = path.join(__dirname, "files");
+const destDir = path.join(__dirname, "files-copy");
+
+copyDir(srcDir, destDir);
+
 
